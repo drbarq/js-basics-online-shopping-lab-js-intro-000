2025b242c5551920d57e47b8fce6d9bc17e3162e
Joe Tustin <j.tustin@gmail.com>
2019-02-19 15:57:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -108,14 +108,8 @@ function removeFromCart(item) {
     if (cart[i].itemName === item) {
       cart.splice(i,1);
     }
-
-
-  else {
-
-
-}
-return `That item is not in your cart.`
-}
+    return `That item is not in your cart.`
+  }
 }
 
 
